a6af847dd3b7c0ce51011160afad443a52142f91
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-02-25 10:14:02 +0530

    modified linearsearch

--- src/Linear_Search/Linear.java
+++ src/Linear_Search/Linear.java
@@ -1,29 +1,24 @@
 package Linear_Search;
-
-import java.util.Scanner;
-
+import  java.util.Scanner;
 class Linear{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int data[] = {13,1,2,3,4,5,12,31,42,54,91,90,98,43};
-        System.out.print("Enter the Id: ");
-        int search = scanner.nextInt();
-        int index = linearSearch(data,search);
-        if(index != -1){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {2,3,4,5,6,7,8,9,0,1,41,32,33,43,56};
+        System.out.print("Enter you number: ");
+        int num = sc.nextInt();
+        int index = LinearSearch(arr,num);
+        if(index != -1)
             System.out.println("Element found at "+index);
-        }
-        else{
-            System.out.println("Element not found ");
-        }
+        else
+            System.out.println("This"+"'"+num+"'"+ "element is not found");
+
     }
 
-    private static int linearSearch(int[] data, int search) {
-        for(int i = 0 ; i < data.length; i++){
-            if(data[i] == search){
+    private static int LinearSearch(int[] arr, int num) {
+        for(int i = 0 ; i < arr.length;i++){
+            if(arr[i] == num){
                 return i;
-
             }
-
         }
         return -1;
     }
