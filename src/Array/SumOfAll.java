4459a42dd90eefb5365e91c56ce1494943eb0272
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-04-02 17:46:00 +0530

    sum task

---
+++ src/Array/SumOfAll.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class SumOfAll {
+    public static void main(String[] args) {
+        int arr[] = {24,5,5};
+        int sum = 0;
+        for(int i= 0 ; i < arr.length;i++){
+//            System.out.println(arr[i]);
+            sum += arr[i];
+
+        }
+        System.out.println(sum);
+    }
+}
