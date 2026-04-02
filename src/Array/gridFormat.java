16e084795ffb7a17c0fbaa9bf6ba8837c4040455
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-04-02 17:59:22 +0530

    grid added

---
+++ src/Array/gridFormat.java
@@ -0,0 +1,26 @@
+package Array;
+class gridFormat{
+    public static void main(String[] args) {
+        int[][] arr = new int[10][10];
+        for(int i = 0 ; i < 10 ; i++){
+            for(int j = 0 ; j < 10; j++){
+                System.out.printf("%2d",arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+//
+////Grid Format using an Array DSA;
+// 0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//         0  0  0  0  0  0  0  0  0  0
+//
+//Process finished with exit code 0
